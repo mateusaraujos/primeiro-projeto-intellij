613cffd439385861746c8fab1f16b7b366df94e2
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2021-08-08 19:52:43 -0300

    Primeiro commit

---
+++ src/app/exemplo/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+// Ctrl + Alt + F10 para começar.
+package app.exemplo;
+
+import app.exemplo.modelo.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        /*System.out.println("Olá, Mundo!");*/
+        Gato gato = new Gato("Pipo", "Branco", 1);
+
+        System.out.println(gato);
+    }
+
+}
---
+++ src/app/exemplo/modelo/Gato.java
@@ -0,0 +1,67 @@
+package app.exemplo.modelo;
+
+import java.util.Objects;
+
+public class Gato {
+
+    /**
+     * Variáveis locais privadas.
+     */
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    public Gato() {}
+
+    /**
+     * Construtor da classe
+     */
+    public Gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Gato gato = (Gato) obj;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{Nome = " + nome + ", Cor = " + cor + ", Idade = " + idade + "}";
+    }
+
+}
